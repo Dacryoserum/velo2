a2b80bb16636c511b7bf1cb02c27c0f7d19a7981
antoinesoulat <antoinesoulat2004@gmail.com>
2024-06-20 10:55:32 +0200

    interface

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -4,15 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { createVuetify } from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
-const vuetify = createVuetify()
-
-createApp(App).use(vuetify).mount('#app')
-
 const app = createApp(App)
 
 app.use(router)
 
-/* app.mount('#app') */
+app.mount('#app')
