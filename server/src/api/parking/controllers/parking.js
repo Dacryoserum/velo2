26637a35ba2630f72122cc0561914805061c33be
Doope12 <hugo.doope12@hotmail.com>
2024-06-20 10:58:54 +0200

    Back

--- server/src/api/parking/controllers/parking.js
+++ server/src/api/parking/controllers/parking.js
@@ -1,6 +1,9 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::parking.parking", ({ strapi }) => ({
+  async find(ctx) {
+    return await super.find(ctx);
+  },
   async findWithinBounds(ctx) {
     const { latBottom, latTop, lngBottom, lngTop } = ctx.query;
 
@@ -14,9 +17,15 @@ module.exports = createCoreController("api::parking.parking", ({ strapi }) => ({
     }
 
     try {
-      const parkings = await strapi
-        .service("api::parking.parking")
-        .findWithinBounds(latBottom, latTop, lngBottom, lngTop);
+      const parkings = await strapi.entityService.findMany(
+        "api::parking.parking",
+        {
+          filters: {
+            X: { $gte: parseFloat(latBottom), $lte: parseFloat(latTop) },
+            Y: { $gte: parseFloat(lngBottom), $lte: parseFloat(lngTop) },
+          },
+        }
+      );
 
       return this.transformResponse(parkings);
     } catch (error) {
--- server/src/api/parking/services/parking.js
+++ server/src/api/parking/services/parking.js
@@ -1,16 +1,9 @@
-"use strict";
+'use strict';
 
 /**
  * parking service
  */
 
-module.exports = {
-  async findWithinBounds(latBottom, latTop, lngBottom, lngTop) {
-    return strapi.entityService.findMany("api::parking.parking", {
-      filters: {
-        X: { $gte: parseFloat(latBottom), $lte: parseFloat(latTop) },
-        Y: { $gte: parseFloat(lngBottom), $lte: parseFloat(lngTop) },
-      },
-    });
-  },
-};
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::parking.parking');
